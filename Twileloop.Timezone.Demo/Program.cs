67fd374d71a66e195d89611f8bab072061508426
Sangeeth Nandakumar <twileloop@outlook.com>
2023-05-17 01:40:49 +0530

    feat - Project restruring and evolving

---
+++ Twileloop.Timezone.Demo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Twileloop.Timezone.Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
